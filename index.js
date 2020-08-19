fddb032394be0cfa51353f971d671d620607ea3a
OSullivan <rbips006@BKUSL-1FJHML7H.local>
2020-08-19 16:33:40 -0400

    the final commit

--- index.js
+++ index.js
@@ -12,31 +12,40 @@ app.use(express.json());
 db.query(`
   CREATE TABLE IF NOT EXISTS clicks(
     id SERIAL PRIMARY KEY,
-    whatGotClicked NOT NULL, 
-    pageX NOT NULL, 
-    pageY NOT NULL,
-    dataId NOT NULL,
-    timestamp NOT NULL,
-    userId NOT NULL,
+    whatGotClicked VARCHAR(128) NOT NULL, 
+    pageX INT NOT NULL, 
+    pageY INT NOT NULL,
+    dataId VARCHAR(128) NOT NULL,
+    timestamp INT NOT NULL,
+    userId VARCHAR(128) NOT NULL
 
   );    
 `);
 
-
-
-
 app.post('/clicks', async (request, response) => {
+  const {
+    whatGotClicked,
+    pageX,
+    pageY,
+    dataId,
+    timestamp,
+    userId,
+  } = request.body;
 
-  const {whatGotClicked, pageX, pageY, dataId, timestamp, userId} = request.body;
-
-  const result = await db.query(
+  await db.query(
     `INSERT INTO clicks (whatGotClicked, pageX, pageY, dataId, timestamp, userId) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
-    [whatGotClicked, pageX, pageY, dataId, timestamp, userId]
+    [
+      whatGotClicked.slice(0, 128),
+      pageX,
+      pageY,
+      dataId,
+      timestamp,
+      Number(userId),
+    ]
   );
 
-  response.json{ click: 'tracked' };
+  response.json({ clicked: 'event' });
 });
-
 
 app.listen(PORT, () =>
   console.log(`Server is up and running at port ${PORT} 🚀`)
